ae2715497d7701f51c7d3ca893cf1660801bba6e
Sam Duckett <samjduckett@gmail.com>
2025-09-03 08:51:37 -0400

    Lab work going back to main

--- src/robot.cpp
+++ src/robot.cpp
@@ -3,7 +3,7 @@
 void Robot::InitializeRobot(void)
 {
     chassis.InititalizeChassis();
-
+    Serial.println("Robot initialized.");
     /**
      * TODO: Set pin 13 HIGH when navigating and LOW when destination is reached.
      * Need to set as OUTPUT here.
@@ -33,6 +33,13 @@ void Robot::RobotLoop(void)
         // We do FK regardless of state
         UpdatePose(velocity);
         chassis.SetMotorEfforts(220,-220);
+    Twist velocity;
+    if(chassis.ChassisLoop(velocity))
+    {
+        // Serial.println("Chassis in CHASSIE LOOP");
+        // We do FK regardless of state
+        UpdatePose(velocity);
+        //chassis.SetMotorEfforts(220,-220);
         
         /**
          * Here, we break with tradition and only call these functions if we're in the 
@@ -46,5 +53,18 @@ void Robot::RobotLoop(void)
             DriveToPoint();
             if(CheckReachedDestination()) HandleDestination();
         }
+    }
+        /**
+         * Here, we break with tradition and only call these functions if we're in the 
+         * DRIVE_TO_POINT state. CheckReachedDestination() is expensive, so we don't want
+         * to do all the maths when we don't need to.
+         * 
+         * While we're at it, we'll toss DriveToPoint() in, as well.
+         */ 
+        if(robotState == ROBOT_DRIVE_TO_POINT)
+        {
+            DriveToPoint();
+            if(CheckReachedDestination()) HandleDestination();
+        }   
     }
 }
